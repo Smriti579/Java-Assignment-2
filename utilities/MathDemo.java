858680c23dc4bc116a5aa0630c23250c58fd5b4a
Smriti579 <smritimahajan579@gmail.com>
2025-02-10 23:47:23 +0530

    Delete utilities directory

--- utilities/MathDemo.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class MathDemo {
-    public static void main(String[] args) {
-        System.out.println("Sum: " + MathUtils.add(10, 5));
-    }
-}
--- utilities/MathUtils.java
+++
@@ -1,7 +0,0 @@
-package utilities;
-
-public class MathUtils {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-}
